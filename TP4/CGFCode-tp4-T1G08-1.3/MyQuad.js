248c74786027ef6093d83d822845d44fe17e4926
Diogo Marques <xmarques999@hotmail.com>
2015-04-10 00:38:47 +0100

    aula prática 4 CHECKPOINT #2

---
+++ TP4/CGFCode-tp4-T1G08-1.3/MyQuad.js
@@ -0,0 +1,41 @@
+function MyQuad(scene)
+{
+    CGFobject.call(this, scene);
+    
+    this.initBuffers();
+};
+
+MyQuad.prototype = Object.create(CGFobject.prototype);
+MyQuad.prototype.constructor = MyQuad;
+
+MyQuad.prototype.initBuffers = function ()
+{
+    this.vertices = [
+        -0.5, -0.5, 0,
+        0.5, -0.5, 0,
+        -0.5, 0.5, 0,
+        0.5, 0.5, 0
+    ];
+
+    this.indices = [
+        0, 1, 2,
+        3, 2, 1
+    ];
+    
+    this.normals = [
+        0, 0, 1,
+        0, 0, 1,
+        0, 0, 1,
+        0, 0, 1,
+    ];
+
+    this.texCoords = [
+       0.0, 1.0,
+       1.0, 1.0,
+      0.0, 0.0,
+      1.0, 0.0,
+    ];
+
+    this.primitiveType = this.scene.gl.TRIANGLES;
+    this.initGLBuffers();
+};
